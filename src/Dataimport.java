31ccbc2c5e7cd845d4d6417a5fde775bdad7afa4
xiaoyi <937640235@qq.com>
2019-04-07 17:00:29 -0400

    search

--- src/Dataimport.java
+++ src/Dataimport.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
 
 public class Dataimport {
 
@@ -11,9 +13,14 @@ public class Dataimport {
         try {
             String urlNameString = url + "?" + param;
             URL realUrl = new URL(urlNameString);
+            // 打开和URL之间的连接
             URLConnection connection = realUrl.openConnection();
             connection.connect();
-            in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            // 获取所有响应头字段
+            // 获取所有响应头字段
+            // 定义 BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
             String line;
             while ((line = in.readLine()) != null) {
                 result += line;
@@ -22,6 +29,7 @@ public class Dataimport {
             System.out.println("发送GET请求出现异常！" + e);
             e.printStackTrace();
         }
+        // 使用finally块来关闭输入流
         finally {
             try {
                 if (in != null) {
@@ -35,7 +43,7 @@ public class Dataimport {
     }
 
     public static void main(String[] args) {
-        String s= Dataimport.sendGet("http://localhost:8983/solr/youthchinacore/dataimport", "command=full-import");
+        String s= Dataimport.sendGet("http://localhost:8983/solr/youthchinacore/Dataimport", "command=full-import");
         System.out.println(s);
     }
 
